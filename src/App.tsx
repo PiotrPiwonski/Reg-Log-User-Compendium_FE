2a026593e149207ddbcbcd4077c616fffbf5b402
mariusz17 <qbajt.mariusz.kubat@gmail.com>
2023-01-04 18:15:11 +0100

    prettier format

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Login from "./pages/Login";
-import SignUp from "./pages/SignUp";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './pages/Login';
+import SignUp from './pages/SignUp';
 
 const App = () => {
   return (
