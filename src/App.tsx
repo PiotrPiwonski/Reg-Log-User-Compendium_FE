376319d82e200f14ff2308d0beb101a0d57b30ac
mariusz17 <qbajt.mariusz.kubat@gmail.com>
2023-01-06 19:36:36 +0100

    add context provider to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,18 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { AuthProvider } from './context/auth/AuthContext';
 import Login from './pages/Login';
 import SignUp from './pages/SignUp';
 
 const App = () => {
-  console.log('husky test 1');
-  console.log('husky test 2');
-  console.log('husky test 3');
-  console.log('husky test 4');
-
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/sign-up" element={<SignUp />} />
-      </Routes>
-    </Router>
+    <AuthProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/sign-up" element={<SignUp />} />
+        </Routes>
+      </Router>
+    </AuthProvider>
   );
 };
 
