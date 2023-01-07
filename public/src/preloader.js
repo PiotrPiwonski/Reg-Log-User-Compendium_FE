8ade812b8bce565c243c0d2485b25d3cea96b09a
isccown125 <92261514+isccown125@users.noreply.github.com>
2023-01-07 17:01:16 +0100

    refactor preloader

--- public/src/preloader.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-console.time();
-window.addEventListener('load',()=>{
-    const preloader = document.querySelector('#preloader');
-    if(document.readyState ==='complete'){
-        return preloader.remove();
-    }
-})
-console.timeEnd();
