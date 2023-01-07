fe4f29fedeaa3c11074225c488a182e170e3eac9
isccown125 <92261514+isccown125@users.noreply.github.com>
2023-01-07 01:19:16 +0100

    added loading spinners components

---
+++ src/components/loading-spinners/PageLoadingSpinner.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './PageLoadingSpinner.css';
+
+const PageLoadingSpinner = () => (
+  <div className="page-backdrop">
+    <div className="default-loader-style page-loader"></div>
+  </div>
+);
+
+export default PageLoadingSpinner;
