2ee23d380735715a867a04185a7f6b9603d128e1
mariusz17 <qbajt.mariusz.kubat@gmail.com>
2023-01-10 13:56:33 +0100

    add auto login with cookie functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,38 @@
+import { useEffect, useContext } from 'react';
+import { getUserWithCookie } from './context/auth/AuthActions';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { AuthProvider } from './context/auth/AuthContext';
+import AuthContext from './context/auth/AuthContext';
 import Login from './pages/Login';
 import SignUp from './pages/SignUp';
 
 const App = () => {
+  const { state: authState, dispatch } = useContext(AuthContext);
+
+  useEffect(() => {
+    (async () => {
+      dispatch({ type: 'SET_LOADING' });
+
+      const user = await getUserWithCookie();
+
+      if (user) {
+        dispatch({ type: 'SET_USER', payload: user });
+      } else {
+        dispatch({ type: 'RESET_USER' });
+      }
+
+      dispatch({ type: 'CLEAR_LOADING' });
+    })();
+  }, [dispatch]);
+
+  if (authState.isLoading) return <h1>Loading...</h1>;
+
   return (
-    <AuthProvider>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/sign-up" element={<SignUp />} />
-        </Routes>
-      </Router>
-    </AuthProvider>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/sign-up" element={<SignUp />} />
+      </Routes>
+    </Router>
   );
 };
 
