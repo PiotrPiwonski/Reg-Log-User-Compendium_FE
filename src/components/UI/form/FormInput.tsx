7873f0d3195611e9474e94acf1a3418001ab21bd
PiotrPiwonski <49531459+PiotrPiwonski@users.noreply.github.com>
2023-01-13 17:18:01 +0100

    add FormInput

---
+++ src/components/UI/form/FormInput.tsx
@@ -0,0 +1,29 @@
+import { HiOutlineUserCircle, HiOutlineEye } from 'react-icons/hi';
+
+interface Props {
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  name: string;
+  type: string;
+  placeholder: string;
+  inputBoxClass: string;
+  labelTitle: string;
+  value: string;
+  reactIcon: string;
+}
+
+const FormInput = ({ onChange, name, type, placeholder, inputBoxClass, labelTitle, value, reactIcon }: Props) => {
+  return (
+    <div className={`input-box ${inputBoxClass}`}>
+      <label htmlFor={name} className="labels">
+        {labelTitle}
+      </label>
+      <div className="input-panel">
+        if (reactIcon === HiOutlineEye) {<HiOutlineEye className="input-icon" />}
+        if (reactIcon === HiOutlineUserCircle) {<HiOutlineUserCircle className="input-icon" />}
+        <input type={type} name={name} id={name} value={value} onChange={onChange} placeholder={placeholder} />
+      </div>
+    </div>
+  );
+};
+
+export default FormInput;
