552b11161e9f1b6bbfc1d6be8c799606bc91a3f4
Przemek <przemyslaw.r.wojciechowski@gmail.com>
2023-01-15 07:13:26 +0100

    Remove PageHeader and div from components

--- src/components/auth/AuthSignUp.tsx
+++ src/components/auth/AuthSignUp.tsx
@@ -1,6 +1,5 @@
 import FormInput from '../UI/form/FormInput';
 import { HiOutlineEye, HiOutlineUserCircle } from 'react-icons/hi';
-import FormHeading from '../UI/form/FormHeading';
 import FormBox from '../UI/form/FormBox';
 
 interface Props {
@@ -12,42 +11,39 @@ interface Props {
 }
 const AuthSignUp = ({ onSubmit, onChange, email, password, repeatPassword }: Props) => {
   return (
-    <div>
-      <FormHeading title="Sign Up" info="Already have an account?" link="/" linkText="Log in here." />
-      <FormBox>
-        <form onSubmit={onSubmit}>
-          <FormInput
-            className="email"
-            type="email"
-            name="email"
-            value={email}
-            onChange={onChange}
-            placeholder="Your Email"
-            labelTitle="Email"
-            IconInput={HiOutlineUserCircle}
-          />
-          <FormInput
-            type="password"
-            name="password"
-            value={password}
-            onChange={onChange}
-            placeholder="Your Password"
-            labelTitle="Password"
-            IconInput={HiOutlineEye}
-          />
-          <FormInput
-            type="password"
-            name="repeatPassword"
-            value={repeatPassword}
-            onChange={onChange}
-            placeholder="Repeat Password"
-            labelTitle="Repeat Password"
-            IconInput={HiOutlineEye}
-            isButtonSubmit={true}
-          />
-        </form>
-      </FormBox>
-    </div>
+    <FormBox>
+      <form onSubmit={onSubmit}>
+        <FormInput
+          className="email"
+          type="email"
+          name="email"
+          value={email}
+          onChange={onChange}
+          placeholder="Your Email"
+          labelTitle="Email"
+          IconInput={HiOutlineUserCircle}
+        />
+        <FormInput
+          type="password"
+          name="password"
+          value={password}
+          onChange={onChange}
+          placeholder="Your Password"
+          labelTitle="Password"
+          IconInput={HiOutlineEye}
+        />
+        <FormInput
+          type="password"
+          name="repeatPassword"
+          value={repeatPassword}
+          onChange={onChange}
+          placeholder="Repeat Password"
+          labelTitle="Repeat Password"
+          IconInput={HiOutlineEye}
+          isButtonSubmit={true}
+        />
+      </form>
+    </FormBox>
   );
 };
 export default AuthSignUp;
