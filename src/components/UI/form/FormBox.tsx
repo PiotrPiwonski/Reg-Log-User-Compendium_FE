1681d3919a03f19b8e7a860b23fbcb4c56b5ec8d
Przemek <przemyslaw.r.wojciechowski@gmail.com>
2023-01-15 07:10:38 +0100

    Remove div

--- src/components/UI/form/FormBox.tsx
+++ src/components/UI/form/FormBox.tsx
@@ -2,11 +2,7 @@ interface Props {
   children: React.ReactNode;
 }
 const FormBox = ({ children }: Props) => {
-  return (
-    <section className="form-box">
-      <div>{children}</div>
-    </section>
-  );
+  return <section className="form-box">{children}</section>;
 };
 
 export default FormBox;
