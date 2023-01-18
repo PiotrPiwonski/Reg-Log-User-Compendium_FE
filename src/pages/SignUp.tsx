7e14339898e168b9d29faf94d8d144893e8bdc5f
PiotrPiwonski <49531459+PiotrPiwonski@users.noreply.github.com>
2023-01-18 18:05:01 +0100

    bug fixes

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -32,64 +32,57 @@ const SignUp = () => {
     // TODO Send to backend
     console.log(formData);
 
-    setLoading(true);
     if (!email) {
       alert('Email jest wymagany.');
-      setLoading(false);
       return;
     }
     if (!password) {
       alert('Hasło jest wymagane.');
-      setLoading(false);
       return;
     }
     if (password.length < 6) {
       alert('Hasło musi mieć przynajmniej 6 znaków.');
-      setLoading(false);
       return;
     }
     if (password.length > 16) {
       alert('Hasło musi mieć maksymalnie 16 znaków.');
-      setLoading(false);
       return;
     }
     if (!repeatPassword) {
       alert('Repeat Password jest wymagane.');
-      setLoading(false);
       return;
     }
     if (repeatPassword !== password) {
       alert('Hasło i jego powtórzenie musi być identyczne');
-      setLoading(false);
       return;
-    } else {
-      try {
-        const res = await fetch('http://localhost:3001/auth/register', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify(formData),
-        });
-        const data = await res.json();
-        const errorMsg = data.message;
-        if (res.status === 201) {
-          alert(`Zarejstrowany użytkownik email: ${data.email} o id: ${data.id}`);
-          return;
-        }
-        if (res.status === 400) {
-          alert(`Błąd podczas rejestracji nowego użytkownika: ${errorMsg}`);
-          return;
-        } else {
-          alert(`Jakiś inny błąd o statusie różnym od 400: ${res.status}`);
-          return;
-        }
-      } catch (error) {
-        alert('Coś nie tak...');
-        console.log('Error: ', error);
-      } finally {
-        setLoading(false);
+    }
+    setLoading(true);
+    try {
+      const res = await fetch('http://localhost:3001/auth/register', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      const errorMsg = data.message;
+      if (res.status === 201) {
+        alert(`Zarejestrowany użytkownik email: ${data.email} o id: ${data.id}`);
+        return;
       }
+      if (res.status === 400) {
+        alert(`Błąd podczas rejestracji nowego użytkownika: ${errorMsg}`);
+        return;
+      } else {
+        alert(`Jakiś inny błąd o statusie różnym od 400: ${res.status}`);
+        return;
+      }
+    } catch (error) {
+      alert('Coś nie tak...');
+      console.log('Error: ', error);
+    } finally {
+      setLoading(false);
     }
   };
 
