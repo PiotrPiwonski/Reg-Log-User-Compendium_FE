0e4c4474e0ed659566a98c78acf52fec8952cf42
PiotrPiwonski <49531459+PiotrPiwonski@users.noreply.github.com>
2023-02-08 16:58:08 +0100

    add css module

--- src/features/LoggedInUser/LoggedInUser.tsx
+++ src/features/LoggedInUser/LoggedInUser.tsx
@@ -1,3 +1,5 @@
+import styles from './LoggedInUser.module.css';
+
 export const LoggedInUser = () => {
-  return <h1>Test LoggedInUser</h1>;
+  return <h1 className={styles.test}>Test LoggedInUser</h1>;
 };
