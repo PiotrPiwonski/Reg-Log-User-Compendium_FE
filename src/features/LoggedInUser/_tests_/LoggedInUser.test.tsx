b1fcc09f9928953a2f1ad8b5ef717f966a6e42a0
PiotrPiwonski <49531459+PiotrPiwonski@users.noreply.github.com>
2023-02-09 16:42:22 +0100

    improving the test

--- src/features/LoggedInUser/_tests_/LoggedInUser.test.tsx
+++ src/features/LoggedInUser/_tests_/LoggedInUser.test.tsx
@@ -1,8 +1,12 @@
 import { render } from '../../../utils/customRender';
 import { LoggedInUser } from '../LoggedInUser';
+import { screen } from '@testing-library/react';
 
-describe('LoggedInUser test', () => {
+describe('LoggedInUser', () => {
   it('should render LoggedInUser', () => {
     render(<LoggedInUser />);
+    const component = screen.getByText('Test LoggedInUser');
+    console.log(component);
+    expect(component).toBeInTheDocument();
   });
 });
