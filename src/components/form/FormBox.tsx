4ad85b0c5e130b0b3e223a7fe98efbd4fae2dc60
Rafal Szczepanik <rafal.szczepanik@grupawp.pl>
2023-02-24 18:06:10 +0100

    refactor(FormBox): implement css module styles

--- src/components/form/FormBox.tsx
+++
@@ -1,8 +0,0 @@
-interface Props {
-  children: React.ReactNode;
-}
-const FormBox = ({ children }: Props) => {
-  return <section className="form-box">{children}</section>;
-};
-
-export default FormBox;
