03befc0e522c38cbcbdfcaf8c659e1b1a577ff77
Rafal Szczepanik <rafal.szczepanik@grupawp.pl>
2023-02-24 18:35:33 +0100

    refactor(Home): implement css modules partly

--- src/features/Home/Home.tsx
+++ src/features/Home/Home.tsx
@@ -1,5 +1,7 @@
 import { useNavigate } from 'react-router-dom';
 
+import styles from './Home.module.css';
+
 import { routes } from '../../routes/routesMap';
 
 export const Home = () => {
@@ -11,7 +13,7 @@ export const Home = () => {
     <div
       data-testid="home-test"
       style={{
-        width: '50vw',
+        height: '100vh',
         display: 'flex',
         flexDirection: 'column',
         justifyContent: 'center',
@@ -20,17 +22,10 @@ export const Home = () => {
     >
       <h1 style={{ fontSize: 50 }}>HOME</h1>
       <div style={{ display: 'flex', gap: 30, marginTop: 40 }}>
-        <button
-          style={{ display: 'flex', position: 'relative', fontSize: 35, width: '100%', padding: 40 }}
-          onClick={() => navigate(routes.signIn)}
-        >
+        <button className={styles.button} onClick={() => navigate(routes.signIn)}>
           Sing In
         </button>
-
-        <button
-          style={{ display: 'flex', position: 'relative', fontSize: 35, width: '100%', padding: 40 }}
-          onClick={() => navigate(routes.signUp)}
-        >
+        <button className={styles.button} onClick={() => navigate(routes.signUp)}>
           Sing Up
         </button>
       </div>
