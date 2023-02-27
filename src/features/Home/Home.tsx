3275f4764a30249c5d3f6053f603c16f1cf13778
sujatwo <117409015+gamol01@users.noreply.github.com>
2023-02-27 17:54:17 +0100

    refactor(Home.tsx): remove redundant lines

--- src/features/Home/Home.tsx
+++ src/features/Home/Home.tsx
@@ -10,22 +10,13 @@ export const Home = () => {
     /* TODO all CSS in JS styles to be moved to CSS Modules*/
   }
   return (
-    <div
-        className={styles.homeWrapper}
-      data-testid="home-test">
-      <h1
-          className={styles.homeTitle}
-      >HOME</h1>
-      <div
-          className={styles.homeButtonWrapper}
-      >
-        <button
-            className={styles.homeButton} onClick={() => navigate(routes.signIn)}
-        >
+    <div className={styles.homeWrapper} data-testid="home-test">
+      <h1 className={styles.homeTitle}>HOME</h1>
+      <div className={styles.homeButtonWrapper}>
+        <button className={styles.homeButton} onClick={() => navigate(routes.signIn)}>
           Sing In
         </button>
-        <button
-            className={styles.homeButton} onClick={() => navigate(routes.signUp)}>
+        <button className={styles.homeButton} onClick={() => navigate(routes.signUp)}>
           Sing Up
         </button>
       </div>
