8070583e9239247d1ae361008c21abdac7a91c5f
PiotrPiwonski <49531459+PiotrPiwonski@users.noreply.github.com>
2023-07-25 16:37:33 +0200

    refactor HandlerUser and add tests

--- src/components/HeaderUser/HeaderUser.tsx
+++ src/components/HeaderUser/HeaderUser.tsx
@@ -1,8 +1,9 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 
 import { HeaderUserProps } from './types';
 import styles from './HeaderUser.module.css';
 import { useLogout } from './useLogout';
+import { useHeaderUser } from './useHeaderUser';
 
 import { arrayAvatar } from '../../utils/avatars';
 import { randomAvatar } from '../../utils/randomAvatar';
@@ -10,42 +11,9 @@ import { randomAvatar } from '../../utils/randomAvatar';
 const avatar = randomAvatar(arrayAvatar);
 
 export const HeaderUser = ({ id, email, role }: HeaderUserProps) => {
-  const [timeLeft, setTimeLeft] = useState(20000);
   const { handleLogOut } = useLogout();
 
-  useEffect(() => {
-    let interval: any;
-
-    const resetTimer = () => {
-      clearInterval(interval);
-      setTimeLeft(20000);
-      startTimer();
-    };
-
-    const startTimer = () => {
-      interval = setInterval(() => {
-        setTimeLeft(timeLeft - 1000);
-      }, 1000);
-    };
-
-    const handleEvent = () => {
-      resetTimer();
-    };
-
-    startTimer();
-
-    window.addEventListener('mousemove', handleEvent);
-    window.addEventListener('keypress', handleEvent);
-    window.addEventListener('click', handleEvent);
-
-    return () => {
-      clearInterval(interval);
-      window.removeEventListener('mousemove', handleEvent);
-      window.removeEventListener('keypress', handleEvent);
-      window.removeEventListener('click', handleEvent);
-    };
-  }, [timeLeft]);
-
+  const timeLeft = useHeaderUser(20);
   useEffect(() => {
     if (timeLeft === 0) {
       handleLogOut();
@@ -61,7 +29,7 @@ export const HeaderUser = ({ id, email, role }: HeaderUserProps) => {
         <h1 className={styles.headerUserInfoTitle}>Witaj użytkowniku (email): {email}</h1>
         <p className={styles.headerUserInfoDetail}>Twoje Id w systemie to: {id}</p>
         <p className={styles.headerUserInfoDetail}>Twoja rola w systemie to: {role}</p>
-        <p className={styles.headerUserInfoDetail}>{timeLeft / 1000}s. - Czas nieaktywności do wylogowania.</p>
+        <p className={styles.headerUserInfoDetail}>{timeLeft}s. - Czas nieaktywności do wylogowania.</p>
       </div>
       <div className={styles.headerUserButtonWrapper}>
         <button className={styles.headerUserButton} onClick={handleLogOut}>
